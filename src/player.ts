1891bbb8625317624057c745560bae47a6b8facb
Julio Milla <julio.milla@slagkryssaren.com>
2018-10-01 15:40:25 +0200

    Preparing to extract more functionality into the player class

--- src/player.ts
+++ src/player.ts
@@ -1,9 +1,42 @@
 export class Player {
 
-  name: string;
-  purse: number = 0;
+  private _name: string;
+  private _purse: number;
+  private _position: number;
+  private _inPenaltyBox: boolean;
 
   constructor(name: string) {
-    this.name = name;
+    this._name = name;
+    this._purse = 0;
+    this._position = 0;
+    this._inPenaltyBox = false;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get purse(): number {
+    return this._purse;
+  }
+
+  get position(): number {
+    return this._position
+  }
+
+  get inPenaltyBox(): boolean {
+    return this._inPenaltyBox;
+  }
+
+  set position(position: number) {
+    this._position = position;
+  }
+
+  set inPenaltyBox(inPenaltyBox: boolean) {
+    this._inPenaltyBox = inPenaltyBox;
+  }
+
+  public incrementPurse() {
+    this._purse += 1;
   }
 }
